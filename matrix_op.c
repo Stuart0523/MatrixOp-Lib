cb6475c8cfb692149a713418f6f88eaba8cb2ace
Stuart0523 <stuart960523@gmail.com>
2025-12-18 16:59:49 +0800

    Add determinant function

--- matrix_op.c
+++ matrix_op.c
@@ -26,3 +26,9 @@ void mat_transpose(double A[SIZE][SIZE], double T[SIZE][SIZE]) {
         }
     }
 }
+double mat_det(double A[SIZE][SIZE]) {
+    return
+        A[0][0] * (A[1][1] * A[2][2] - A[1][2] * A[2][1])
+      - A[0][1] * (A[1][0] * A[2][2] - A[1][2] * A[2][0])
+      + A[0][2] * (A[1][0] * A[2][1] - A[1][1] * A[2][0]);
+}
