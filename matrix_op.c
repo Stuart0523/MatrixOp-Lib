8b9764c5a61f57ce7bd9e0e90b00bdf48854c64d
Stuart0523 <stuart960523@gmail.com>
2025-12-18 16:54:51 +0800

    Implement matrix multiplication

--- matrix_op.c
+++ matrix_op.c
@@ -7,3 +7,14 @@ void mat_elem_mul(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZ
         }
     }
 }
+
+void mat_mul(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < SIZE; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
