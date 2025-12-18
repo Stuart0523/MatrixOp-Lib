35f8cf9ead932c5242b573a6f981675ae25c2784
Stuart0523 <stuart960523@gmail.com>
2025-12-18 17:00:49 +0800

    Add adjoint matrix function

--- matrix_op.c
+++ matrix_op.c
@@ -32,3 +32,23 @@ double mat_det(double A[SIZE][SIZE]) {
       - A[0][1] * (A[1][0] * A[2][2] - A[1][2] * A[2][0])
       + A[0][2] * (A[1][0] * A[2][1] - A[1][1] * A[2][0]);
 }
+
+void mat_adj(double A[SIZE][SIZE], double adj[SIZE][SIZE]) {
+    // Cofactor matrix (not transposed yet)
+    double c00 =  (A[1][1]*A[2][2] - A[1][2]*A[2][1]);
+    double c01 = -(A[1][0]*A[2][2] - A[1][2]*A[2][0]);
+    double c02 =  (A[1][0]*A[2][1] - A[1][1]*A[2][0]);
+
+    double c10 = -(A[0][1]*A[2][2] - A[0][2]*A[2][1]);
+    double c11 =  (A[0][0]*A[2][2] - A[0][2]*A[2][0]);
+    double c12 = -(A[0][0]*A[2][1] - A[0][1]*A[2][0]);
+
+    double c20 =  (A[0][1]*A[1][2] - A[0][2]*A[1][1]);
+    double c21 = -(A[0][0]*A[1][2] - A[0][2]*A[1][0]);
+    double c22 =  (A[0][0]*A[1][1] - A[0][1]*A[1][0]);
+
+    // adj(A) = transpose(cofactor(A))
+    adj[0][0] = c00; adj[0][1] = c10; adj[0][2] = c20;
+    adj[1][0] = c01; adj[1][1] = c11; adj[1][2] = c21;
+    adj[2][0] = c02; adj[2][1] = c12; adj[2][2] = c22;
+}
